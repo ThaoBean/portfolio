959d739d54d397499870d2aa83e01f2c21ef4d83
thaodo <thaobean1701@gmail.com>
2026-05-22 17:46:31 +0700

    hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import { About } from "../components/sections/about/about";
-import { Contact } from "../components/sections/contact/contact";
-import { ExperienceTimeline } from "../components/sections/experience/experience-timeline";
-import { FeaturedProjects } from "../components/sections/featured-projects";
-import { Hero } from "../components/sections/hero";
-import { Skills } from "../components/sections/skills/skills";
+// import { About } from "../components/sections/about/about";
+// import { Contact } from "../components/sections/contact/contact";
+// import { ExperienceTimeline } from "../components/sections/experience/experience-timeline";
+// import { FeaturedProjects } from "../components/sections/featured-projects";
+// import { Hero } from "../components/sections/hero";
+// import { Skills } from "../components/sections/skills/skills";
 
 export default function HomePage() {
   return (
     <main className='min-h-screen bg-[#0B0F19] text-white'>
-      <Hero />
+      Thao Do
+      {/* <Hero />
       <About />
       <FeaturedProjects />
       <Skills />
       <ExperienceTimeline />
-      <Contact />
+      <Contact /> */}
     </main>
   );
 }
